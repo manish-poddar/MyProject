be62bda65f190a32c85a3e4ff8f9f74d324f29af
manish-poddar <manish.poddar@hindustantimes.com>
2015-10-08 17:12:49 +0530

    start

---
+++ app/src/main/java/com/example/manish/myproject/LoginActivity.java
@@ -0,0 +1,79 @@
+package com.example.manish.myproject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class LoginActivity extends Activity {
+
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private TextView info;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.activity_login);
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        info = (TextView) findViewById(R.id.info);
+
+
+
+  loginButton.setReadPermissions("user_friends");
+
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                );// App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+
+                info.setText("login cancelled");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                info.setText("login failed");
+            }
+
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
